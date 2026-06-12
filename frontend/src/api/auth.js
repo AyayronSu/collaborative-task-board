e8296f5223b72869acaa654b6a1700568a510acf
AyayronSu <aarondsujana@gmail.com>
2026-06-12 14:57:22 -0500

    Created auth.js

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,6 @@
+import client from "./client";
+
+export const signup  = (data) => client.post('/api/auth/signup', data)
+export const login   = (data) => client.post('/api/auth/login'  data)
+export const logout  = ()     => client.post('/api/auth/logout')
+export const getMe   = ()     => client.get('/api/auth/me')
